8ed87047c5a092b378bca595abd0df309b012634
ylee <ylee@student.42seoul.kr>
2022-02-16 18:11:43 +0900

    small error fix in reverse_iterator

--- utils/reverse_iterator.hpp
+++ utils/reverse_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:48:04 by ylee              #+#    #+#             */
-/*   Updated: 2022/02/10 17:21:18 by ylee             ###   ########.fr       */
+/*   Updated: 2022/02/16 18:09:03 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ namespace	ft
 	template<typename Iter>
 	class	reverse_iterator
 	{
-		typedef	Iter											iterator_type;
+	public:
+		typedef	Iter													iterator_type;
 		typedef typename ft::iterator_traits<Iter>::iterator_category	iterator_category;
 		typedef typename ft::iterator_traits<Iter>::value_type			value_type;
 		typedef typename ft::iterator_traits<Iter>::difference_type		difference_type;
@@ -40,37 +41,37 @@ namespace	ft
 			current = other.current ;
 			return *this ;
 		}
-		iterator_type	base()
+		iterator_type	base() const
 		{
 			return reverse_iterator<Iter>(current).current ;
 		}
 		reference			operator*() const
 		{
 			iterator_type	tmp = current ;
-			return *--tmp ;
+			return *(--tmp) ;
 		}
 		pointer				operator->() const { return &(operator*()) ; }
 		reference			operator[]( difference_type	n ) const { return base()[-n-1] ; }
 		reverse_iterator&	operator++()
 		{
-			--(*this) ;
-			return this ;
+			current-- ;
+			return *this ;
 		}
 		reverse_iterator	operator++(int)
 		{
 			reverse_iterator	tmp = *this ;
-			--(*this);
+			++(*this);
 			return tmp ;
 		}
 		reverse_iterator&	operator--()
 		{
-			++(*this) ;
-			return this ;
+			current++ ;
+			return *this ;
 		}
 		reverse_iterator	operator--(int)
 		{
 			reverse_iterator	tmp = *this ;
-			++(*this);
+			--(*this);
 			return tmp ;
 		}
 		reverse_iterator	operator+(difference_type n) const
