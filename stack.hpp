a0fb2c7e29825fdc631494eaa00ee26c732fb687
ylee <ylee@student.42seoul.kr>
2022-02-20 17:53:05 +0900

    fix small error

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:44:11 by ylee              #+#    #+#             */
-/*   Updated: 2022/02/20 17:21:40 by ylee             ###   ########.fr       */
+/*   Updated: 2022/02/20 17:51:44 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,32 @@
 namespace	ft
 {
     template < typename T, typename Container = ft::vector<T> >
-    class stack
+    class   stack
     {
     public:
         typedef Container                   container_type;
-        typedef Container::value_type       value_type;
-        typedef Container::size_type        size_type;
-        typedef Container::reference        reference;
-        typedef Container::const_reference  const_reference;
+        typedef typename Container::value_type       value_type;
+        typedef typename Container::size_type        size_type;
+        typedef typename Container::reference        reference;
+        typedef typename Container::const_reference  const_reference;
         
     protected:
         container_type  c;
     public:
-        explicit stack( const Container& cont = Container() )
-        : c(cont) {}
-        
+        explicit stack( const Container& cont = Container() ):c(cont)
+        {}
         stack( const stack& other )
         {
             *this = other ;
         }
-        
         stack& operator=( const stack& other )
         {
             this->c = other.c ;
             return *this ;
         }
         ~stack()
-        {
-            ~c();
-        }
-        
-        reference top();
+        {}
+        reference   top()
         {
             return  c.back();
         }
@@ -56,40 +51,40 @@ namespace	ft
         {
             return  c.back();
         }
-        bool empty() const
+        bool    empty() const
         {
-            return  c.empty();
+            return  this->c.empty();
         }
-        size_type size() const
+        size_type   size() const
         {
-            return  c.size();
+            return  (this->c.size()) ;
         }
-        void push( const value_type& value )
+        void    push( const value_type& value )
         {
             c.push_back(value);
         }
-        void pop()
+        void    pop()
         {
             c.pop_back();
         }
         
-        template< class T, class Container >
-        friend bool operator==( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs );
+        template< class U, class Cont >
+        friend bool operator==( const ft::stack<U,Cont>& lhs, const ft::stack<T,Container>& rhs );
 
-        template< class T, class Container >
-        friend bool operator!=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs );
+        template< class U, class Cont >
+        friend bool operator!=( const ft::stack<U,Cont>& lhs, const ft::stack<T,Container>& rhs );
     
-        template< class T, class Container >
-        friend bool operator<( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs );
+        template< class U, class Cont >
+        friend bool operator< ( const ft::stack<U,Cont>& lhs, const ft::stack<T,Container>& rhs );
     
-        template< class T, class Container >
-        friend bool operator<=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs );
+        template< class U, class Cont >
+        friend bool operator<=( const ft::stack<U,Cont>& lhs, const ft::stack<T,Container>& rhs );
     
-        template< class T, class Container >
-        friend bool operator>( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs );
+        template< class U, class Cont >
+        friend bool operator> ( const ft::stack<U,Cont>& lhs, const ft::stack<T,Container>& rhs );
     
-        template< class T, class Container >
-        friend bool operator>=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs );
+        template< class U, class Cont >
+        friend bool operator>=( const ft::stack<U,Cont>& lhs, const ft::stack<T,Container>& rhs );
 
     };
 
