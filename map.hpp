071b7fa383def89e65f3706dc3611599ec9248c2
ylee <ylee@student.42seoul.kr>
2022-03-15 14:57:59 +0900

    add map&tree functions

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:44:11 by ylee              #+#    #+#             */
-/*   Updated: 2022/03/11 00:44:36 by ylee             ###   ########.fr       */
+/*   Updated: 2022/03/15 14:45:24 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MAP_HPP
 
 # include <memory>
+# include <stdexcept>
 # include "./utils/binary_search_tree.hpp"
 # include "./utils/pair.hpp"
 # include "./utils/less.hpp"
@@ -37,8 +38,8 @@ namespace	ft
 		typedef const value_type&									const_reference;
 		typedef typename Allocator::pointer									pointer;
 		typedef typename Allocator::const_pointer							const_pointer;
-		typedef typename ft::binary_search_tree<value_type>::iterator		iterator;
-		typedef typename ft::binary_search_tree<value_type>::const_iterator	const_iterator;
+		typedef typename ft::binary_search_tree<value_type, Compare>::iterator		iterator;
+		typedef typename ft::binary_search_tree<value_type, Compare>::const_iterator	const_iterator;
 		typedef typename ft::reverse_iterator<iterator>						reverse_iterator;
 		typedef typename ft::reverse_iterator<const_iterator>				const_reverse_iterator;
 	
@@ -60,67 +61,267 @@ namespace	ft
 		};
 		
 	private:
-		ft::binary_search_tree<value_type, value_compare >    tree;
+		allocator_type			alloc;
+		key_compare				comp;
+		ft::binary_search_tree<value_type, Compare>    tree;
 
 	public:
-		map(Key& key = Key(), T& mapped = T()):tree(value_type(key,mapped)) {}
-		// explicit map( const Compare& comp, const Allocator& alloc = Allocator() )
-		// {}
-		// template< class InputIt >
-		// map( InputIt first, InputIt last, const Compare& comp = Compare(), const Allocator& alloc = Allocator() )
-		// {}
-		// map( const map& copy )
-		// {}
-		// ~map(){}
-		// map&    operator=(const map& copy)
-		// {}
-		allocator_type get_allocator() const;
-		T& at( const Key& key );
-		const T& at( const Key& key ) const;
-		T& operator[]( const Key& key );
-		iterator begin(){return (this->tree).begin() ; }
-		const_iterator begin() const;
-		iterator end(){ return	(this->tree).end() ; }
-		const_iterator end() const;
-		reverse_iterator rbegin();
-		const_reverse_iterator rbegin() const;
-		reverse_iterator rend();
-		const_reverse_iterator rend() const;
-		bool empty() const;
-		size_type size() const;
-		size_type max_size() const;
-		void clear(); 
-		ft::pair<iterator, bool> insert( const value_type& value )
-		{return	tree.insert(value) ; }
-		template< class InputIt >
-		void insert( InputIt first, InputIt last );
+		map()
+		: alloc(Allocator()), comp(Compare())
+		{}
 		
-		void erase( iterator pos ); 
-		void erase( iterator first, iterator last );
-		size_type erase( const Key& key );
-		void swap( map& other );
-		size_type count( const Key& key ) const;
+		explicit map( const Compare& comp, const Allocator& alloc = Allocator() )
+		: alloc(alloc), comp(comp)
+		{
+			tree = ft::binary_search_tree<value_type, Compare>(comp);
+		}
+		
+		template< class InputIt >
+		map( InputIt first, InputIt last, const Compare& comp = Compare(), const Allocator& alloc = Allocator() )
+		: alloc(alloc), comp(comp)
+		{
+			tree = ft::binary_search_tree<value_type, Compare>(comp);
+			tree.insert(first, last) ;
+		}
+		
+		map( const map& copy )
+		: alloc(copy.alloc), comp(copy.comp), tree(copy.tree)
+		{}
+		
+		~map()
+		{
+			clear();
+		}
+		
+		map&    operator=(const map& copy)
+		{
+			alloc = copy.alloc;
+			comp = copy.comp;
+			tree = copy.tree;
+			return *this ;
+		}
+		
+		allocator_type get_allocator() const
+		{
+			return	alloc ;
+		}
+		
+		T& at( const Key& key )
+		{
+			iterator	k = find(key);
+			if (k == end())
+				throw (std::out_of_range("not found key") ) ;
+			return	(k->value).second ;
+		}
+		
+		const T& at( const Key& key ) const
+		{
+			iterator	k = find(key);
+			if (k == end())
+				throw (std::out_of_range("not found key") ) ;
+			return	(k->value).second ;
+		}
+		
+		T& operator[]( const Key& key )
+		{
+			iterator	k = find(key);
+			if (k == end())
+			{
+				k = insert(value_type(key)).first;
+			}
+			return	(k->value).second ;
+		}
+		
+		iterator begin()
+		{
+			return iterator(tree.beginNode(), tree.endNode()) ;
+		}
+		
+		const_iterator begin() const
+		{
+			return	const_iterator(tree.beginNode(), tree.endNode()) ;
+		}
+		
+		iterator end()
+		{
+			return	tree.end() ;
+		}
+		
+		const_iterator end() const
+		{
+			return	const_iterator(tree.endN, tree.endN) ;
+		}
+		
+		reverse_iterator rbegin()
+		{
+			return	reverse_iterator(end()) ;
+		}
+		
+		const_reverse_iterator rbegin() const
+		{
+			return	const_reverse_iterator(end()) ;
+		}
+		
+		reverse_iterator rend()
+		{
+			return	reverse_iterator(begin()) ;
+		}
+		
+		const_reverse_iterator rend() const
+		{
+			return	reverse_iterator(begin()) ;
+		}
+		
+		bool empty() const
+		{
+			if (begin() == end())
+				return true ;
+			return false ;
+		}
+		size_type size() const
+		{
+			return	tree.size() ;
+		}
+		size_type max_size() const
+		{
+			return	tree.max_size() ;
+		}
+		
+		void clear()
+		{
+			// iterator	i = begin();
+			// while (*i != tree.endN)
+			// {
+			// 	erase(i);
+			// 	i++;
+			// }
+			tree.clear();
+		}
+		
+		ft::pair<iterator, bool> insert( const value_type& value )
+		{
+			return	tree.insert(value) ;
+		}
+		
+		template< class InputIt >
+		void insert( InputIt first, InputIt last )
+		{
+			tree.insert(first, last) ;
+		}
+		
+		void erase( iterator pos )
+		{
+			tree.erase(pos) ;
+		}
+		
+		void erase( iterator first, iterator last )
+		{
+			tree.erase(first, last) ;
+		}
+		
+		size_type erase( const Key& key )
+		{
+			return	tree.erase(value_type(key)) ;
+		}
+		
+		void swap( map& other )
+		{
+			ft::binary_search_tree<value_type>	tmp(tree) ;
+			tree = other.tree ;
+			other.tree = tmp ;
+		}
+		
+		size_type count( const Key& key ) const
+		{
+			return	tree.count(value_type(key)) ;
+		}
+		
 		iterator find( const Key& key )
 		{
 			return	tree.find(value_type(key)) ;
 		}
-		const_iterator find( const Key& key ) const;
-		ft::pair<iterator,iterator> equal_range( const Key& key );
-		ft::pair<const_iterator,const_iterator> equal_range( const Key& key ) const;
-		iterator lower_bound( const Key& key );
-		const_iterator lower_bound( const Key& key ) const;
-		iterator upper_bound( const Key& key );
-		const_iterator upper_bound( const Key& key ) const;
-		key_compare key_comp() const;
+		
+		const_iterator find( const Key& key ) const
+		{
+			return	tree.find(value_type(key)) ;
+		}
+		
+		ft::pair<iterator,iterator> equal_range( const Key& key )
+		{
+			return	tree.equal_range(value_type(key)) ;
+		}
+		
+		ft::pair<const_iterator,const_iterator> equal_range( const Key& key ) const
+		{
+			return	tree.equal_range(value_type(key)) ;
+		}
+		
+		iterator lower_bound( const Key& key )
+		{
+			return	tree.lower_bound(value_type(key)) ;
+		}
+		
+		const_iterator lower_bound( const Key& key ) const
+		{
+			return	tree.lower_bound(value_type(key)) ;
+		}
+		
+		iterator upper_bound( const Key& key )
+		{
+			return	tree.upper_bound(value_type(key)) ;
+		}
+		
+		const_iterator upper_bound( const Key& key ) const
+		{
+			return	tree.upper_bound(value_type(key)) ;
+		}
+		
+		key_compare key_comp() const
+		{
+			return comp ;
+		}
+		
 		typename ft::map<Key, T>::value_compare value_comp() const
 		{
 			return value_compare() ;
 		}
+		
 		template< class K, class U, class Comp, class Al >
-		friend bool operator==( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs ); 
+		friend bool operator==( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs );
+
+		template< class K, class U, class Comp, class Al >
+		friend bool operator!=( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs );
+
+		template< class K, class U, class Comp, class Al >
+		friend bool operator< ( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs );
+
+		template< class K, class U, class Comp, class Al >
+		friend bool operator<=( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs );
+
+		template< class K, class U, class Comp, class Al >
+		friend bool operator> ( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs );
+
+		template< class K, class U, class Comp, class Al >
+		friend bool operator>=( const ft::map<K,U,Comp,Al>& lhs, const ft::map<K,U,Comp,Al>& rhs ); 
 	};
+	
 	template< class Key, class T, class Compare, class Alloc >
 	bool operator==( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs ); 
+	
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator!=( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs ); 
+	
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator< ( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs ); 
+	
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<=( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs ); 
+	
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator> ( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs ); 
+	
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>=( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs ); 
 	
 	template< class Key, class T, class Compare, class Alloc >
 	void swap( ft::map<Key,T,Compare,Alloc>& lhs, ft::map<Key,T,Compare,Alloc>& rhs );
--- utils/binary_search_tree.hpp
+++ utils/binary_search_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:32:49 by ylee              #+#    #+#             */
-/*   Updated: 2022/03/10 23:51:12 by ylee             ###   ########.fr       */
+/*   Updated: 2022/03/15 14:50:40 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,27 @@ namespace   ft
 	class binary_search_tree
 	{
 	public:
-		typedef ft::BST_iterator< Node >					iterator;
-		typedef	ft::BST_iterator<const Node >				const_iterator;
-		typedef	ft::reverse_iterator<iterator>			reverse_iterator;
-		typedef	ft::reverse_iterator<const_iterator>	const_reverse_iterator;
-		typedef	size_t	size_type;
-	private:
+		typedef typename ft::BST_iterator< Node, Compare >					iterator;
+		typedef	typename ft::BST_const_iterator< Node, Compare >				const_iterator;
+		typedef	typename ft::reverse_iterator<iterator>			reverse_iterator;
+		typedef	typename ft::reverse_iterator<const_iterator>	const_reverse_iterator;
+		typedef	size_t										size_type;
+
 		Node_Alloc	node_alloc;
 		Node*		root;
 		Node*		endN;
-		Compare		value_comp;
+		Compare		key_comp;
 		size_type	len ;
-	public:
-		binary_search_tree()
-		: node_alloc(Node_Alloc()), value_comp(Compare()), len(0)
+
+		binary_search_tree(Compare comp = Compare(), Node_Alloc alloc = Node_Alloc())
+		: node_alloc(alloc), key_comp(comp), len(0)
 		{
 			endN = node_alloc.allocate(1);
 			node_alloc.construct(endN, Node(T(), endN, endN, endN));
 			root = endN ;
 		}
-		binary_search_tree(const T& val)
-		: node_alloc(Node_Alloc()), value_comp(Compare()), len(1)
+		binary_search_tree(const T& val, Compare comp = Compare(), Node_Alloc alloc = Node_Alloc())
+		: node_alloc(alloc), key_comp(comp), len(1)
 		{
 			endN = node_alloc.allocate(1);
 			node_alloc.construct(endN, Node(T(), endN, endN, endN));
@@ -59,14 +59,14 @@ namespace   ft
 		}
 		
 		binary_search_tree(const binary_search_tree& copy)
-		: node_alloc(Node_Alloc()), value_comp(Compare())
+		: node_alloc(copy.node_alloc), key_comp(copy.key_comp)
 		{
 			*this = copy;
 		}
 		
 		~binary_search_tree()
 		{
-			// clear();
+			clear();
 			node_alloc.destroy(endN);
 			node_alloc.deallocate(endN, 1);
 		}
@@ -78,47 +78,67 @@ namespace   ft
 			endN = node_alloc.allocate(1);
 			node_alloc.construct(endN, *(copy.endN));
 		}
-		
-		iterator	begin()
+
+		Node*		beginNode()
 		{
 			Node*	tmp = root;
 			while (tmp->left != endN)
 			{
+				// std::cout << "this node : " << *tmp << std::endl;
 				tmp = tmp->left;
 			}
-			return iterator(tmp, endN) ;
+			return tmp ;
 		}
-
-		iterator	lastNode()
+		
+		Node*	lastNode()
 		{
 			Node*	tmp = root;
 			while (tmp->right != endN)
 			{
 				tmp = tmp->right;
 			}
-			return iterator(tmp, endN) ;
+			return tmp ;
 		}
-		
+
+		Node*	endNode()
+		{
+			return	endN ;
+		}
+
+		iterator	begin()
+		{
+			return iterator(beginNode(), endN) ;
+		}
+
+		const_iterator	begin() const
+		{
+			return const_iterator(beginNode(), endN) ;
+		}
+
 		iterator	end()
 		{
 			return iterator(endN, endN) ;
 		}
+
+		const_iterator	end() const
+		{
+			return const_iterator(endN, endN) ;
+		}
 		
 		iterator	find(const T& key)
 		{
-			//root 부터 시작해서 찾는거로 수정하기
-			iterator	i = begin();
-			while (*i != endN)
+			Node*	i = root ;
+			while (i != endN)
 			{
-				std::cout << "this Node : " << *i << "//\n";
-				if (value_comp(**i, key))
-					i++;
-				else if (value_comp(key, **i))
-					return end() ;
+				// std::cout << "this Node : " << i->value << "//\n";
+				if (key_comp((i->value).first, key.first))
+					i = i->right ;
+				else if (key_comp(key.first, (i->value).first))
+					i = i->left ;
 				else
-					return i ;
+					return iterator(i, endN);
 			}
-			return i ;
+			return iterator(i, endN) ;
 		}
 
 		iterator	find(const Node& keyN)
@@ -131,36 +151,164 @@ namespace   ft
 			Node*	tmp = root ;
 			Node*	prev = tmp ;
 			bool	moveLeft = true ;
+			bool	biggest = true ;
 
 			while (tmp != endN)
 			{
-				if (value_comp(**tmp, value)) // value 가 큰 경우
+				if (key_comp((tmp->value).first, value.first)) // value 가 큰 경우
 				{
 					prev = tmp ;
 					tmp = tmp->right ;
 					moveLeft = false ;
 				}
-				else if (value_comp(value, **tmp)) // value 가 작은 경우
+				else if (key_comp(value.first, (tmp->value).first)) // value 가 작은 경우
 				{
 					prev = tmp ;
 					tmp = tmp->left ;
 					moveLeft = true ;
+					biggest = false ;
 				}
 				else // tmp 의 key 와 value 의 key 가 같은 경우
 					return ft::pair<iterator, bool>(end(), false) ;
 			}
 			Node*	newNode = node_alloc.allocate(1);
 			node_alloc.construct(newNode, Node(value, prev, endN, endN)) ;
-			if (moveLeft == true)
-				prev->left = newNode ;
-			else
-				prev->right = newNode ;
+			if (prev != endN)
+			{
+				if (moveLeft == true)
+					prev->left = newNode ;
+				else
+					prev->right = newNode ;
+			}
+			else if (prev == endN || biggest)
+				endN->parent = newNode ;
+			
 			if (len == 0)
 				root = newNode ;
 			len++;
+			//insert check
+			iterator	i = begin();
+			iterator	f = end();
+				std::cout << ">>insert " << value << " result <<\n" ;
+			while (i!=f)
+			{
+				std::cout << *i << std::endl;
+				i++;
+			}
+			std::cout << *i << "\t>>end" << std::endl;
+			
 			return ft::pair<iterator, bool>(iterator(newNode, endN), true) ;
 		}
+
+		size_type	size()
+		{
+			return	len ;
+		}
+
+		size_type	max_size()
+		{
+			return	node_alloc.max_size() ;
+		}
 		
+		void	clear()
+		{
+			iterator	i = begin() ;
+			iterator	tmp = i;
+			iterator	f = end() ;
+			while (i != f)
+			{
+				tmp = i++;
+				erase(tmp) ;
+			}
+			erase(i) ;
+		}
+
+		void erase( iterator pos )
+		{
+			if (pos == end())
+			 	return ;
+			std::cout << "erase node : " << *pos << std::endl;
+			Node*	eraseN = pos.base();
+			Node*	parentN = eraseN->parent ;
+			Node*	replaceN = endN ;
+			bool	leftNode = (parentN->left == eraseN) ;
+			// 지울 노드가 parent 의 left 였다면 leftNode == true, right 였다면 false
+			if (eraseN->left == endN && eraseN->right == endN) // leaf 인 경우
+			{
+				if (leftNode)
+					parentN->left = endN ;
+				else
+					parentN->right = endN ;
+				if (eraseN == root)
+					root = endN ;
+			}
+			else if (eraseN->left != endN && eraseN->right != endN) // 자식 노드가 2개 있는 경우
+			{
+				replaceN = eraseN->left;
+				while (replaceN->right != endN)
+					replaceN = replaceN->right ;
+				// left 노드 중 가장 큰 노드의 값으로 대체한뒤 가장 큰 left 노드를 제거한다.
+				if (replaceN->parent == eraseN)
+				{
+					eraseN->left = eraseN ;
+					replaceN->parent = replaceN ;
+				}
+				Node	tmp(*replaceN);
+				replaceN->copyExceptValue(*eraseN);
+				eraseN->copyExceptValue(tmp) ;
+				if (eraseN == root)
+					root = replaceN ;
+				erase(iterator(eraseN, endN));
+				return ;
+			}
+			else // 자식 노드가 1개인 경우
+			{
+				replaceN = (eraseN->left == endN) ? eraseN->right : eraseN->left ;
+				if (leftNode)
+				{
+					if (parentN != endN)
+						parentN->left = replaceN ;
+					replaceN->parent = parentN ;
+				}
+				else
+				{
+					if (parentN != endN)
+						parentN->right = replaceN ;
+					replaceN->parent = parentN ;
+				}
+				if (eraseN == root)
+					root = replaceN ;
+			}
+			node_alloc.destroy(eraseN);
+			node_alloc.deallocate(eraseN, 1);
+			len--;
+			endN->parent = lastNode();
+			//정렬
+		}
+		
+		void erase( iterator first, iterator last )
+		{
+			iterator	tmp = first;
+			iterator	next = tmp ;
+			while (next != end() && next != last)
+			{
+				tmp = next++;
+				std::cout << "tmp : " << *tmp << " , next : " << *next << std::endl;
+				erase(tmp);
+			}
+		}
+		
+		size_type erase( const T& key )
+		{
+			size_type	cnt = 0;
+			iterator	findN = find(key) ;
+			if (findN != end())
+			{
+				erase(findN);	
+				cnt++;
+			}
+			return	cnt ;
+		}
 	};
 }
 
