cf95b30e98847fea85d259b29d0a497ce67db633
ylee <ylee@student.42seoul.kr>
2022-03-15 14:56:51 +0900

    modify endNode to use in reverse_iterator add BST_const_iterator but cannot operate yet

--- utils/BST_iterator.hpp
+++ utils/BST_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:48:04 by ylee              #+#    #+#             */
-/*   Updated: 2022/03/10 22:09:19 by ylee             ###   ########.fr       */
+/*   Updated: 2022/03/15 03:44:38 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,8 @@ namespace	ft
 		: value(value_type(value)), parent(parent), left(left), right(right)
 		{}
 		BST_Node(const BST_Node& copy)
-		{
-			*this = copy;
-		}
+		:value(copy.value), parent(copy.parent), left(copy.left), right(copy.right)
+		{}
 		BST_Node&	operator=(const BST_Node& copy)
 		{
 			value = copy.value;
@@ -55,6 +54,14 @@ namespace	ft
 		value_type	operator*()
 		{
 			return value ;
+		}
+		
+		BST_Node&	copyExceptValue(const BST_Node& copy)
+		{
+			parent = copy.parent;
+			left = copy.left;
+			right = copy.right;
+			return *this ;
 		}
 		
 		bool	operator==(const BST_Node& other)
@@ -128,11 +135,13 @@ namespace	ft
 	template <typename T>
 	std::ostream&	operator<<(std::ostream& out, const BST_Node<T>& node)
 	{
-		out << node.value;
+		out << "this node information\n";
+		out << "\t> value : " << node.value << "\n";
+		out << "\t> parent : " << (node.parent)->value << "\n";
+		out << "\t> left : " << (node.left)->value << "\n";
+		out << "\t> right : " << (node.right)->value << "\n";
 		return out ;
 	}
-	
-
 
 	template<typename Node, typename Compare = ft::less<Node> >
 	class	BST_iterator : ft::iterator<ft::bidirectional_iterator_tag, Node>
@@ -156,7 +165,7 @@ namespace	ft
 		{}
 		BST_iterator( const BST_iterator& other )
 		{ *this = other ; }
-		~BST_iterator() {}
+		virtual ~BST_iterator() {}
 		BST_iterator&	operator=( const BST_iterator& other)
 		{
 			curN = other.curN ;
@@ -191,12 +200,12 @@ namespace	ft
 			else if (curN != endN)
 			{
 				tmp = curN->parent ;
-				while (tmp != endN && comp(*tmp, *curN))
+				while (tmp != endN && comp(tmp->value.first, curN->value.first))
 					tmp = tmp->parent ;
 				curN = tmp ;
 			}
 			else
-				curN = curN->right ;
+				curN = curN->parent ;
 			return *this ;
 		}
 		BST_iterator	operator++(int)
@@ -218,12 +227,12 @@ namespace	ft
 			else if (curN != endN)
 			{
 				tmp = curN->parent ;
-				while (tmp != endN && comp(*curN, *tmp))
+				while (tmp != endN && comp(curN->value.first, tmp->value.first))
 					tmp = tmp->parent ;
 				curN = tmp ;
 			}
 			else
-				curN = curN->left ;
+				curN = curN->parent ;
 			return *this ;
 		}
 		BST_iterator	operator--(int)
@@ -233,19 +242,24 @@ namespace	ft
 			return tmp ;
 		}
 
+		// operator	ft::BST_const_iterator<Node, Compare> () const
+		// {
+		// 	return	ft::BST_const_iterator<Node, Compare>(curN, endN) ;
+		// }
+
 	};
 	//non-member function operators
-	template<typename Node>
-	bool					operator==(const BST_iterator<Node>& lhs,
-										const BST_iterator<Node>& rhs)
+	template<typename Node, typename Compare>
+	bool					operator==(const BST_iterator<Node, Compare>& lhs,
+										const BST_iterator<Node, Compare>& rhs)
 	{
-		return *lhs.base() == *rhs.base() ;
+		return *(lhs.base()) == *(rhs.base()) ;
 	}
-	template<typename Node>
-	bool					operator!=(const BST_iterator<Node>& lhs,
-										const BST_iterator<Node>& rhs)
+	template<typename Node, typename Compare>
+	bool					operator!=(const BST_iterator<Node, Compare>& lhs,
+										const BST_iterator<Node, Compare>& rhs)
 	{
-		return *lhs.base() != *rhs.base() ;
+		return !(lhs == rhs) ;
 	}
 	template<typename Node>
 	bool					operator<(const BST_iterator<Node>& lhs,
@@ -268,6 +282,147 @@ namespace	ft
 	template<typename Node>
 	bool					operator>=(const BST_iterator<Node>& lhs,
 										const BST_iterator<Node>& rhs)
+	{
+		return !(*lhs.base() < *rhs.base()) ;
+	}
+
+
+	template<typename Node, typename Compare = ft::less<Node> >
+	class	BST_const_iterator : ft::iterator<ft::bidirectional_iterator_tag, Node>
+	{
+	public:
+		typedef typename ft::iterator<ft::bidirectional_iterator_tag, Node>::iterator_category	iterator_category;
+		typedef typename ft::iterator<ft::bidirectional_iterator_tag, Node>::value_type			value_type;
+		typedef typename ft::iterator<ft::bidirectional_iterator_tag, Node>::difference_type	difference_type;
+		typedef Node*			pointer;
+		typedef Node&			reference;
+	protected:
+		pointer	curN ;
+		pointer	endN ;
+		Compare	comp ;
+	public:
+		BST_const_iterator()
+		: curN(nullptr), endN(nullptr), comp(Compare())
+		{}
+		explicit	BST_const_iterator(pointer curN, pointer endN = nullptr, Compare comp = Compare())
+		: curN(curN), endN(endN), comp(comp)
+		{}
+		BST_const_iterator( const BST_const_iterator& other )
+		{ *this = other ; }
+		virtual ~BST_const_iterator() {}
+		BST_const_iterator&	operator=( const BST_const_iterator& other)
+		{
+			curN = other.curN ;
+			endN = other.endN ;
+			comp = other.comp ;
+			return *this ;
+		}
+		pointer	base() const
+		{
+			return curN ;
+		}
+		
+		reference			operator*() const
+		{
+			return *curN ;
+		}
+		
+		pointer				operator->() const
+		{
+			return &(operator*()) ;
+		}
+		
+		BST_const_iterator&	operator++()
+		{
+			pointer	tmp = curN;
+			//std::cout << "this Node : " << tmp << "//";
+			if (curN->right != endN)
+			{
+				tmp = curN->right ;
+				while (tmp->left != endN)
+					tmp = tmp->left ;
+				curN = tmp ;
+			}
+			else if (curN != endN)
+			{
+				tmp = curN->parent ;
+				while (tmp != endN && comp(tmp->value.first, curN->value.first))
+					tmp = tmp->parent ;
+				curN = tmp ;
+			}
+			else
+				curN = curN->right ;
+			return *this ;
+		}
+		BST_const_iterator	operator++(int)
+		{
+			BST_const_iterator	tmp = *this ;
+			operator++() ;
+			return tmp ;
+		}
+		BST_const_iterator&	operator--()
+		{
+			pointer	tmp = curN;
+			if (curN->left != endN)
+			{
+				tmp = curN->left ;
+				while (tmp->right != endN)
+					tmp = tmp->right ;
+				curN = tmp ;
+			}
+			else if (curN != endN)
+			{
+				tmp = curN->parent ;
+				while (tmp != endN && comp(curN->value.first, tmp->value.first))
+					tmp = tmp->parent ;
+				curN = tmp ;
+			}
+			else
+				curN = curN->left ;
+			return *this ;
+		}
+		BST_const_iterator	operator--(int)
+		{
+			BST_const_iterator	tmp = *this ;
+			operator--() ;
+			return tmp ;
+		}
+
+	};
+	//non-member function operators
+	template<typename Node, typename Compare>
+	bool					operator==(const BST_const_iterator<Node, Compare>& lhs,
+										const BST_const_iterator<Node, Compare>& rhs)
+	{
+		return *lhs.base() == *rhs.base() ;
+	}
+	template<typename Node, typename Compare>
+	bool					operator!=(const BST_const_iterator<Node, Compare>& lhs,
+										const BST_const_iterator<Node, Compare>& rhs)
+	{
+		return *lhs.base() != *rhs.base() ;
+	}
+	template<typename Node, typename Compare>
+	bool					operator<(const BST_const_iterator<Node, Compare>& lhs,
+										const BST_const_iterator<Node, Compare>& rhs)
+	{
+		return  *lhs.base() < *rhs.base() ;
+	}
+	template<typename Node>
+	bool					operator<=(const BST_const_iterator<Node>& lhs,
+										const BST_const_iterator<Node>& rhs)
+	{
+		return !(*rhs.base() < *lhs.base()) ;
+	}
+	template<typename Node>
+	bool					operator>(const BST_const_iterator<Node>& lhs,
+										const BST_const_iterator<Node>& rhs)
+	{
+		return *rhs.base() < *lhs.base() ;
+	}
+	template<typename Node>
+	bool					operator>=(const BST_const_iterator<Node>& lhs,
+										const BST_const_iterator<Node>& rhs)
 	{
 		return !(*lhs.base() < *rhs.base()) ;
 	}
